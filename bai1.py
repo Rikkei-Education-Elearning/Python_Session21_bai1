d160cd7c9a0ac0c23f77e993b12f176e4c5238f6
Minh Quân Hoàng <quanhoagn@gmail.com>
2026-06-15 21:34:26 +0700

    bai1

--- bai1.py
+++ bai1.py
@@ -20,6 +20,25 @@ def deposit(amount):
     except ValueError:
         logging.error("Wrong type of data!")
 
+def transfer(amount):
+    global Balance
+    if "-" in amount:
+        logging.error("Money can't be negative!")
+        print("Không được nhập số âm!")
+        return 
+    try: 
+        if int(amount) > Balance:
+            logging.warning(f"Transfer failed! Not enough balance to transfer {amount}VND. Current balance: {Balance:,}")
+            print("Số dư không đủ để chuyển tiền!")
+            return 
+        if int(amount) >= 10000000:
+            logging.info(f"Large transfer detected! Attempting to transfer {amount}VND. Current balance: {Balance:,}")
+        Balance -= int(amount)
+        logging.info(f"Transfer successful -{amount}VND. Current balance: {Balance:,}")
+        print(f"Chuyển tiền thành công! số dư mới {Balance:,} VND")
+    except ValueError:
+        logging.error("Wrong type of data!")
+
 while True:
     print("VÍ MOMO GIẢ LẬP".center(50,"="))
     print("""
@@ -34,3 +53,11 @@ while True:
         case '1':
             money_amount = input("Nhập vào số tiền muốn nạp: ")
             deposit(money_amount)
+        case '2':
+            money_amount = input("Nhập vào số tiền muốn chuyển: ")
+            transfer(money_amount)
+        case '3':
+            logging.info(f"Checked balance: {Balance:,} VND")
+            print(f"Số dư hiện tại: {Balance:,} VND")
+        case '4':
+            break
