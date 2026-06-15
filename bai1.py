a325a5603db2206ccb546c16c45219f14d769ff4
Minh Quân Hoàng <quanhoagn@gmail.com>
2026-06-15 21:29:13 +0700

    bai1

---
+++ bai1.py
@@ -0,0 +1,36 @@
+Balance = 0
+import logging 
+
+logging.basicConfig(
+    filename='momo_transactions.log', 
+    level=logging.INFO, 
+    format='[%(asctime)s] - [%(levelname)s] - [%(message)s]'
+    )
+
+def deposit(amount):
+    global Balance
+    if "-" in amount:
+        logging.error("Money can't be negative!")
+        print("Không được nhập số âm!")
+        return 
+    try: 
+        Balance += int(amount)
+        logging.info(f"Deposit successful +{amount}VND. Current balance: {Balance:,}")
+        print(f"Nạp tiền thành công! số dư mới {Balance:,} VND")
+    except ValueError:
+        logging.error("Wrong type of data!")
+
+while True:
+    print("VÍ MOMO GIẢ LẬP".center(50,"="))
+    print("""
+        1. Nạp tiền vào ví
+        2. Chuyển tiền 
+        3. Xem số dư hiện tại 
+        4. Thoát  
+    """)
+    print("=" * 50)
+    choice = input("nhập vào lựa chọn (1 - 4):")
+    match choice:
+        case '1':
+            money_amount = input("Nhập vào số tiền muốn nạp: ")
+            deposit(money_amount)
